fc80c61592d8ef853a5dd0e2102a28735fee1c45
Oussama Khiar <ossama.khiar@gmail.com>
2024-05-26 14:36:21 +0100

    Landing page sm:design

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,11 +7,11 @@ const LandingPage = () => {
         <div className="w-screen h-screen">
             <img alt="background" src="/imgs/landing_page_bg.jpg" className="w-full h-full object-cover absolute top-0" />
 
-            <div className="w-full h-full flex justify-around relative bg-black bg-opacity-40">
+            <div className="w-full h-full flex justify-center relative bg-black bg-opacity-40">
                 <div className="w-1/2 flex flex-col items-start justify-center">
-                    <h1 className="text-9xl text-deep-plum">Meet</h1>
-                    <h2 className="poetsen-one-regular text-6xl text-white">Your Perfect Match with Matcha!</h2>
-                    <div className="text-white text-xl my-5">
+                    <h1 className="text-7xl lg:text-9xl text-deep-plum">Meet</h1>
+                    <h2 className="poetsen-one-regular text-2xl lg:text-6xl text-white">Your Perfect Match with Matcha!</h2>
+                    <div className="text-white text-md lg:text-xl my-5">
                         <p className="font-semibold">
                             Are you ready to meet new people and find that special someone? Matcha is here to help.
                             Our smart matching algorithm and easy-to-use interface make finding love simple and fun.
